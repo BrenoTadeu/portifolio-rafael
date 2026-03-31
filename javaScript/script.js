2e90f248c16e663ea8861ee1ee5a2b9ee3d29516
BrenoTadeu <btgs1233@gmail.com>
2026-03-31 13:13:58 -0300

    adicionando new codes

--- javaScript/script.js
+++ javaScript/script.js
@@ -6,32 +6,42 @@
 const text = "Eai! Me chamo Rafael e seja bem-vindo ao meu portfólio!";
 let i = 0;
 
-function escrever(){
-    if(i < text.length){
+function escrever() {
+    if (i < text.length) {
         document.getElementById("text").innerHTML += text.charAt(i);
         i++
-        setTimeout(escrever,70);
-    }else{
+        setTimeout(escrever, 70);
+    } else {
         animationShowProjects();
     }
 }
 
-function animationShowProjects(){
+document.addEventListener ("DOMContentLoaded",() =>{
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    });
+});
+
+document.body.classList.add("travado");
+
+function animationShowProjects() {
     setTimeout(() => {
         document.querySelector(".projetos").classList.add("show");
         document.querySelector(".centerIndex").classList.add("moving");
         document.getElementById("text").style.textAlign = "start";
+        document.body.classList.remove("travado");
     }, 200);
 }
 
 escrever();
 
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach(entry =>{
-        if(entry.isIntersecting){entry.target.classList.add("show");}
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) { entry.target.classList.add("show"); }
     })
 })
 
-document.querySelectorAll(".hidden").forEach(el =>{
+document.querySelectorAll(".hidden").forEach(el => {
     observer.observe(el);
 })
